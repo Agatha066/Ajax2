1593b39edc44d80847191bf914b5ab2d8d4d514b
Agatha066 <53305979+Agatha066@users.noreply.github.com>
2021-05-25 10:27:22 -0300

    Update cidades.php

--- cidades.php
+++ cidades.php
@@ -5,19 +5,20 @@
 		$estado = $_GET['estado'];
 
 		if ($estado == "RJ" || $estado == "rj")
-    {
-				echo '{"estado": ["Rio de Janeiro", "Niteroi", "Angra", "Caxias"]} ';
-    }
-    else
-    {
-      if ($estado == "SP" || $estado == "sp")
-      {
-				echo '{"estado": ["Sao Paulo", "Guarulhos", "Jundiai", "Campinas"]} ';
-      }
-		  else
-      {
-				echo "Voce inseriu um estado invalido!! ";
-      }
+    		{
+	        	echo '{"estado": ["Rio de Janeiro", "Niteroi", "Angra", "Caxias"]} ';
+		}
+		else
+		{
+		      if ($estado == "SP" || $estado == "sp")
+		      {
+		      	echo '{"estado": ["Sao Paulo", "Guarulhos", "Jundiai", "Campinas"]} ';
+		      }
+		      else
+		      {
+			 echo '{"estado": ["Vazio"]} ';
+		      	//echo "Voce inseriu um estado invalido!! ";
+		      }
 		}	
 		
 	}
